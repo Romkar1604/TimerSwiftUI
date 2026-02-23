493ce1f9758f9d02c033bd62810c62c5cfad8662
Omkar <omkar.ranawade98@gmail.com>
2026-02-23 09:54:53 -0500

    Initial Commit

---
+++ TimerSwiftUI/TimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUIApp.swift
+//  TimerSwiftUI
+//
+//  Created by Omkar Ranawade on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerSwiftUITests/TimerSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUITests.swift
+//  TimerSwiftUITests
+//
+//  Created by Omkar Ranawade on 2/23/26.
+//
+
+import Testing
+@testable import TimerSwiftUI
+
+struct TimerSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
